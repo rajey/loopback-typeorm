901d69bd6fbe00fcc64796c7585a8d79f88f1e5d
Rajab Mkomwa <rajeyw@gmail.com>
2019-02-10 16:58:55 +0300

    fix: correct issues that prevented auto creation of database tables

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,16 @@
-import {Entity} from "typeorm";
+import {Entity, PrimaryGeneratedColumn, Column} from 'typeorm';
 
 @Entity()
 export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
 
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  age: number;
 }
